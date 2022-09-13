439bd6bb36c4e9b04230c137de47599c0d32a8bc
pjc1991 <whdekf@gmail.com>
2022-09-13 19:12:11 +0900

    FEAT : now consider minus time

--- very-test-code/go_home_timer.py
+++ very-test-code/go_home_timer.py
@@ -6,6 +6,9 @@ def main():
     go_home_time = input("What time do you want to go home? (HH:MM) ")
     go_home_time = datetime.datetime.strptime(go_home_time, "%H:%M").time()
     minutes = calculate_time_until_go_home(go_home_time)
+    if(minutes < 0):
+        print("You should have left already!")
+        return
     hours, minutes = minutes_to_hours_minutes(minutes)
     print("You will go home at: {}, Time until go home: {} hours and {} minutes until home".format(go_home_time, hours, minutes))
 
@@ -13,7 +16,7 @@ def calculate_time_until_go_home(go_home_time):
     now = datetime.datetime.now()
     go_home = datetime.datetime.combine(now.date(), go_home_time)
     if now > go_home:
-        go_home += datetime.timedelta(days=1)
+        return -1
     return (go_home - now).seconds // 60
 
 def minutes_to_hours_minutes(minutes):
