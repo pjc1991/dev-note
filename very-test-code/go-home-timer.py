75b2d9c372b567b516f9b5a7d976c5550cbb139a
pjc1991 <whdekf@gmail.com>
2022-09-13 17:41:22 +0900

    style : change the order of function

--- very-test-code/go-home-timer.py
+++ very-test-code/go-home-timer.py
@@ -1,5 +1,14 @@
 import datetime
 
+
+def main():
+    # GET go home time from user
+    go_home_time = input("What time do you want to go home? (HH:MM) ")
+    go_home_time = datetime.datetime.strptime(go_home_time, "%H:%M").time()
+    minutes = calculate_time_until_go_home(go_home_time)
+    hours, minutes = minutes_to_hours_minutes(minutes)
+    print("You will go home at: {}, Time until go home: {} hours and {} minutes until home".format(go_home_time, hours, minutes))
+
 def calculate_time_until_go_home(go_home_time):
     now = datetime.datetime.now()
     go_home = datetime.datetime.combine(now.date(), go_home_time)
@@ -9,14 +18,6 @@ def calculate_time_until_go_home(go_home_time):
 
 def minutes_to_hours_minutes(minutes):
     return minutes // 60, minutes % 60
-
-def main():
-    # GET go home time from user
-    go_home_time = input("What time do you want to go home? (HH:MM) ")
-    go_home_time = datetime.datetime.strptime(go_home_time, "%H:%M").time()
-    minutes = calculate_time_until_go_home(go_home_time)
-    hours, minutes = minutes_to_hours_minutes(minutes)
-    print("You will go home at: {}, Time until go home: {} hours and {} minutes until home".format(go_home_time, hours, minutes))
 
 if __name__ == "__main__":
     main()
