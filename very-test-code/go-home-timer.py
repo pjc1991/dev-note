431a1f5d57fd4ecc52af77549187e69cc95a7da8
pjc1991 <whdekf@gmail.com>
2022-09-13 13:12:05 +0900

    FEAT : calculate the time until home.

---
+++ very-test-code/go-home-timer.py
@@ -0,0 +1,22 @@
+import datetime
+
+def calculate_time_until_go_home(go_home_time):
+    now = datetime.datetime.now()
+    go_home = datetime.datetime.combine(now.date(), go_home_time)
+    if now > go_home:
+        go_home += datetime.timedelta(days=1)
+    return (go_home - now).seconds // 60
+
+def minutes_to_hours_minutes(minutes):
+    return minutes // 60, minutes % 60
+
+def main():
+    # GET go home time from user
+    go_home_time = input("What time do you want to go home? (HH:MM) ")
+    go_home_time = datetime.datetime.strptime(go_home_time, "%H:%M").time()
+    minutes = calculate_time_until_go_home(go_home_time)
+    hours, minutes = minutes_to_hours_minutes(minutes)
+    print("Time until go home: {} hours and {} minutes until home".format(hours, minutes))
+
+if __name__ == "__main__":
+    main()
